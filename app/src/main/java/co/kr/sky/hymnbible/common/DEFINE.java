1ed2cab91d7191c51407a1c3f40bac29dc9d176b
Sim KyouYong <SKY@Sim-ui-MacBook-Pro.local>
2017-11-10 16:13:47 +0900

    회사컴터로 커밋

---
+++ app/src/main/java/co/kr/sky/hymnbible/common/DEFINE.java
@@ -0,0 +1,12 @@
+package co.kr.sky.hymnbible.common;
+
+public class DEFINE {
+
+    /*
+    * 구글 계정 : miline0531@gmail.com
+    * */
+    public static final String SERVER_URL       = "http://hoon86.cafe24.com";
+    public static final String SERVER_WEB_URL       = SERVER_URL + "/index.do?";
+
+
+}
